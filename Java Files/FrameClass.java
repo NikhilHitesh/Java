51d82a8432f29b810c001600dbfa3403f801c04c
NikhilHitesh <hiteshchouhan8209@gmail.com>
2023-08-17 15:06:03 +0530

    Create a button and label

--- Java Files/FrameClass.java
+++ Java Files/FrameClass.java
@@ -1,30 +1,43 @@
 package framepack;
+import framepack.btn;
 // import swing lib
 import javax.swing.*;
-import java.awt.*;
+// import java.awt.*;
+// import java.awt.event.*;
 
 public class FrameClass{
 
-	public static void frame(){
+	public JFrame frame = new JFrame();
+	public JButton button;
+	public JLabel label;
+	public JTextField textfield;
+
+	public FrameClass(){
+
 		// System.out.println("Hello Shiv Ji and Rajveer This Code Is Directly Puch From Editor");
 	
-		JFrame frame = new JFrame();
+
+		// frame.addWindowListener(new WindowAdapter(){public void windowClosing(WindowEvent e){exitForm(e);}});
 		// frame.setDafaultCloseOperation(JFrame.EXIT_ON_CLOSE); ---> Not Working.
+
 		frame.setVisible(true);
 		frame.setTitle("My Frame");
+
 		// frame.setSize(600,500);
 		// frame.setLocation(200,100); 
-		frame.setBounds(200,100,600,500);
+
+		frame.setBounds(200,100,400,400);
+		frame.setLayout(null);
+
 		// ImageIcon icon = new ImageIcon("icon.png");
 		// frame.setIconImage(icon.getImage()); ---> Not Working In Linux Mey Be Work In Windows.
 		// Container c = frame.getContentPane();
 		// c.setBackground(Color.RED);
 		// frame.setResizable(false); ----> Not Working In Linux Mey Be Work In Windows.
-
-	}
+}
 
 	public static void main(String[] args) {
-		
-		// frame();
+		// new FrameClass();
+		// FrameClass obj = new FrameClass();
 	}
 }
--- Java Files/MainClass.java
+++ Java Files/MainClass.java
@@ -1,12 +1,13 @@
-// import custome package
 import framepack.FrameClass;
+// import custome package
 // import swing package
-import javax.swing.*;
+// import javax.swing.*;
 
 public class MainClass{
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		FrameClass obj = new FrameClass();
-		obj.frame();
+		// FrameClass obj = new FrameClass();
+		// obj.frame();
+		new FrameClass();
 	}
 }
